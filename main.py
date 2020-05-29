987d922feba8a0eaf3d12257dbc45cf640d00f2a
Abhishek Bvs <bvsabhishek@gmail.com>
2020-05-29 20:24:16 +0530

    Add Threshold and Count window

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/video_feed')
-def video_feed():
+@app.route('/main_video_feed')
+def main_video_feed():
     return Response(gen(VideoCamera()),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
